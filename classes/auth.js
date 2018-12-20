6202fe091d6b9690a648ba92578099124046b129
kylin <kylinsmail@gmail.com>
2018-12-20 14:43:34 +0800

    permissions check

--- classes/auth.js
+++ classes/auth.js
@@ -3,9 +3,9 @@ const Utility = require('./utility');
 const Shop = require('./shops');
 const jwt = require('jwt-simple');
 const jwtSecret = require('../config/global.json').jwtSecret;
-const expireDuration = 5 * 60; // 5*60s
+const expireDuration = 5 * 60 * 60; // 5*60s
 const logger = require('../log');
-const httpStatusCode = require('./htt_status_code');
+const httpStatusCode = require('./http_status_code');
 const Auth = class {
     constructor(username, password) {
         this._name = username;
@@ -21,17 +21,19 @@ const Auth = class {
     }
 
     async Login() {
-        const user = await new User(this._name).InitAsync();
+        const user = await new User({
+            name: this._name
+        }).InitAsync();
         if (user && user.password == this._password) {
             let shop = await (new Shop(user.shopId)).InitAsync();
             return {
                 success: true,
                 status: 200,
-                response: {
+                content: {
                     token: this._EncodeToken({
                         userid: user.id,
                         username: user.name,
-                        role: user.roleNames,
+                        roles: user.roleNames,
                         shop: user.shopId
                     })
                 }
@@ -40,7 +42,7 @@ const Auth = class {
             return {
                 success: false,
                 status: httpStatusCode['Unauthorized'],
-                response: Utility.MakeErrorResponse({
+                content: Utility.MakeErrorResponse({
                     id: 0,
                     detail: "用户名或密码错误。",
                 })
@@ -58,7 +60,7 @@ const Auth = class {
             return {
                 success: true,
                 status: 200,
-                response: {
+                content: {
                     token: refreshToken
                 }
             };
@@ -87,9 +89,20 @@ const Auth = class {
             };
             logger.info(`iat:${iat},exp:${exp},userid:${userid},roles:${roles},
                          username:${username},shop:${shop},customer:${customer}`)
-            let user = await new User(username).InitAsync();
-            let permissions = await user.GetPermissions();
-
+            let user = await new User({
+                name: username
+            }).InitAsync();
+            let permissions = await user.GetPermissionsAsync({
+                Action: req.method.toUpperCase(),
+            });
+            for (let permission of permissions) {
+                let re = new RegExp(permission.Path);
+                if (re.test(req.path))
+                    return {
+                        success: true
+                    };
+            }
+            return this._InvalidTokenResponse('Unauthorized', '无权限进行操作');
         } else {
             logger.warn(decodedToken);
             return this._InvalidTokenResponse('Unauthorized', decodedToken.jwterror);
@@ -100,10 +113,10 @@ const Auth = class {
         return {
             success: false,
             status: httpStatusCode[status],
-            response: Utility.MakeErrorResponse({
+            content: Utility.MakeErrorResponse({
                 id: 0,
                 detail: detail
-            })
+            }).content
         }
     }
 
@@ -112,7 +125,6 @@ const Auth = class {
             return undefined;
         }
         let array = header.authorization.split(' ');
-        console.log(array)
         if (array.length != 2) {
             return undefined;
         }
